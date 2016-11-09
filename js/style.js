61ab2af1cbf969e67e6018332d8d5eb5e7605b89
Alice <alice.wu.nz@gmail.com>
2016-11-10 12:07:44 +1300

    Change menu content and add smooth scroll effect

--- js/style.js
+++ js/style.js
@@ -15,7 +15,24 @@ $('.button-collapse').sideNav({
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
-      
+
+
+// Smooth scroll effect
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
 // Image scroll down
 
